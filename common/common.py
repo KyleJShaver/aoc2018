c1121d068abbe809b8b855087eb76ceba6f4f0ef
Kyle Shaver <kyleshaver@me.com>
2018-12-03 14:37:00 -0800

    added duration to bothparts call

--- common/common.py
+++ common/common.py
@@ -1,4 +1,5 @@
 from typing import Callable
+import time
 
 
 def getinput(filename: str = "input.txt", directory: str = "") -> str:
@@ -9,5 +10,8 @@ def getinput(filename: str = "input.txt", directory: str = "") -> str:
 
 def bothparts(part1: Callable, part2: Callable) -> None:  # pragma: no cover
     """ Calls the provided functions with no arguments """
+    start: float = time.time()
     print(f"Part 1: {part1()}")
     print(f"Part 2: {part2()}")
+    end: float = time.time()
+    print("\nRan in {:.4f} seconds".format(end - start))
